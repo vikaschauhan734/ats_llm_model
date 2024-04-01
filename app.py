15afead7ce9de7cd6fdcd96cfeeed6acad770196
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-04-01 21:17:52 +0530

    added complete pdf for parsing

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import re
 import base64
 from flask import Flask, request, render_template
-import os
-import io
+import os, sys, io, re
+from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Check if the GOOGLE_API_KEY is provided via command line argument
+if len(sys.argv) < 2:
+    print("Please provide the Google API key as a command line argument.")
+    sys.exit(1)
+
+# Extract the API key from command line argument
+google_api_key = sys.argv[1]
+
+genai.configure(api_key=google_api_key)
 
 def get_gemini_response(input, pdf_content, prompt):
     model = genai.GenerativeModel('gemini-pro-vision')
@@ -19,22 +26,35 @@ def get_gemini_response(input, pdf_content, prompt):
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        ## Convert pdf to image
+        # Convert pdf to images
         images = pdf2image.convert_from_bytes(uploaded_file.read())
         
-        first_page = images[0]
-
-        #Convert to bytes
-        img_byte_arr = io.BytesIO()
-        first_page.save(img_byte_arr, format='JPEG')
-        img_byte_arr = img_byte_arr.getvalue()
-
-        pdf_parts = [
-            {
-                "mime_type": "image/jpeg",
-                "data": base64.b64encode(img_byte_arr).decode() #encode to base64
-            }
-        ]
+        # Determine the dimensions of the combined image
+        max_width = max(image.width for image in images)
+        total_height = sum(image.height for image in images)
+        
+        # Create a new blank image to hold the combined pages
+        combined_image = Image.new('RGB', (max_width, total_height))
+        
+        # Paste each page onto the combined image
+        current_y = 0
+        for image in images:
+            combined_image.paste(image, (0, current_y))
+            current_y += image.height
+        
+        # Save the combined image as bytes
+        combined_image_bytes = io.BytesIO()
+        combined_image.save(combined_image_bytes, format='JPEG')
+        combined_image_bytes.seek(0)
+        
+        # Encode combined image to base64
+        combined_image_base64 = base64.b64encode(combined_image_bytes.getvalue()).decode()
+        
+        pdf_parts = [{
+            "mime_type": "image/jpeg",
+            "data": combined_image_base64
+        }]
+        
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
